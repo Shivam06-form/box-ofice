a40067c6b9f6b8ec5cf3ab24527a3e8ca2e9d5f9
Shivam <shiavm@gmail.com>
2021-05-12 17:12:12 +0530

    display show result and small fetch refactor

---
+++ src/components/misc/config.js
@@ -0,0 +1,8 @@
+const API_BASED_URL ='http://api.tvmaze.com';
+
+export async function apiGet(queryString){
+
+    const responce = await fetch(`${API_BASED_URL}${queryString}`).then(r=>r.json())
+  return responce ;
+
+}
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,25 @@
+
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 
+import { render } from '@testing-library/react';
 import React,{useState} from 'react';
+import { createRenderer } from 'react-dom/test-utils';
 import MainPageLayout from '../MainPageLayout';
+import{apiGet} from '../misc/config'; 
 
 
 
 const Home = () => {
 const [input ,setInput] = useState(' ');
 
+const [results,setResults]= useState(null);
 
 const onSearch  = () =>{
-  // https://api.tvmaze.com/search/shows?q=men
-fetch(`http://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>{
-  console.log(result);
+  apiGet(`http://api.tvmaze.com/search/shows?q=${input}`).then(result =>{
+    setResults(result); 
+    console.log(result);  
 });
-
 };
 
 const onInputChange = ev =>{
@@ -27,15 +31,31 @@ const onKeyDown = (ev) => {
    
     onSearch ()
   }
-
-
 };
+
+const renderResultS = ()=>{
+
+  if(results && results.length === 0 ){
+    return<div>No results</div>
+  }
+
+  if(results && results.length > 0){
+   return <div>{results.map((itme)=>(
+   <div key={itme.show.id}>{itme.show.name}</div> ) )} </div> 
+  }
+
+   return null ;
+
+
+}
 
 
 return (
   <MainPageLayout>
       <input type = "text" onChange={onInputChange} onKeyDown={onKeyDown}  value={input}/>
-       <button type="button" onClick={onSearch}>Search</button>
+       <button type="button" onClick={onSearch}>Search
+       </button>
+       {renderResultS()}
     </MainPageLayout>
       
   );
