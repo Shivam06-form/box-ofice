69f426db5447b18228a4e4fa9173311fd011afef
Shivam <shiavm@gmail.com>
2021-05-15 11:44:14 +0530

    Updates

---
+++ precache-manifest.e2881d5970589cc9fa1bca95b95ba400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5fe7dda41c9dd368d2adedb443565e",
+    "url": "/box-ofice%20/index.html"
+  },
+  {
+    "revision": "1413aa006b34e34b5d42",
+    "url": "/box-ofice%20/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "3a13f9d8de7c4d786b41",
+    "url": "/box-ofice%20/static/js/2.21327754.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-ofice%20/static/js/2.21327754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1413aa006b34e34b5d42",
+    "url": "/box-ofice%20/static/js/main.a18b4ea2.chunk.js"
+  },
+  {
+    "revision": "7f1ff285b04eea5c7715",
+    "url": "/box-ofice%20/static/js/runtime-main.4cee4e37.js"
+  }
+]);
