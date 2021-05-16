48a7bb7746485ecc9402d4c7ddb4f1b4fb23cc7b
Shivam <shiavm@gmail.com>
2021-05-16 10:00:51 +0530

    Updates

--- precache-manifest.ec4341fb7c736793265d3e1eadbbb581.js
+++ precache-manifest.ec4341fb7c736793265d3e1eadbbb581.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f5fe7dda41c9dd368d2adedb443565e",
+    "revision": "0e1b1b5505c16aa22ad5a5cbae2d938e",
     "url": "/box-ofice%20/index.html"
   },
   {
-    "revision": "1413aa006b34e34b5d42",
+    "revision": "3d98e712d3256707a4a3",
     "url": "/box-ofice%20/static/css/main.93052401.chunk.css"
   },
   {
-    "revision": "3a13f9d8de7c4d786b41",
-    "url": "/box-ofice%20/static/js/2.21327754.chunk.js"
+    "revision": "91015fa7e5327e92d371",
+    "url": "/box-ofice%20/static/js/2.55389839.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/box-ofice%20/static/js/2.21327754.chunk.js.LICENSE.txt"
+    "url": "/box-ofice%20/static/js/2.55389839.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1413aa006b34e34b5d42",
-    "url": "/box-ofice%20/static/js/main.a18b4ea2.chunk.js"
+    "revision": "3d98e712d3256707a4a3",
+    "url": "/box-ofice%20/static/js/main.379d0b3f.chunk.js"
   },
   {
     "revision": "7f1ff285b04eea5c7715",
